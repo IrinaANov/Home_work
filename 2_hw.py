c0c1182b4c867241fd468d1a0ab9fc023fba71ad
IrinaANov <ira1981nov@yandex.ru>
2024-08-28 18:34:09 +0300

    Update 2_hw.py

--- 2_hw.py
+++ 2_hw.py
@@ -1,1 +1,37 @@
+Задача 1
+
+def task_1 () -> None:
+  a: int = 5
+  b: float = 1.5
+  c: str - 'Hello'
+  d: list = [1, 2, 3, 4, 5]
+  e: bool = True
+
+print(type ('a'))
+print(type ('b'))
+print(type ('c'))
+print(type ('d'))
+print(type ('e'))
+
+task_1()
+
+
+Задача 2
+
+def task_2():
+    return a
+
+a = [1, 2, 3, 5, 8, 13, 21]
+
+print('a[0:3] = ', a[0:3])
+
+task_2()
+Это последовательность Фибоначчи
+
+
+Задача 3
+def task_3(number: int) -> int:
+    return number * number
+
+print(task_3(5))
 
