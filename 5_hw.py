043a6ff8575cfc1fb2174148ecc0d4ca0a7e81d7
IrinaANov <ira1981nov@yandex.ru>
2024-09-13 20:15:14 +0300

    Update 5_hw.py

--- 5_hw.py
+++ 5_hw.py
@@ -1,33 +1,27 @@
 from selenium import webdriver
-
-driver = webdriver.Chrome()
-driver.get("https://www.saucedemo.com/")
-
-
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 driver.get("https://www.saucedemo.com/")
 
-
 username_field = driver.find_element(By.CSS_SELECTOR, "input#user-name")
+password_field = driver.find_element(By.CSS_SELECTOR, "input#password")
+submit_button = driver.find_element(By.CSS_SELECTOR, "[type='submit']")
 
+assert username_field.is_displayed()
+assert password_field.is_displayed()
+assert submit_button.is_displayed()
+
+     
 if username_field:
     print('Элементы найдены')
 else:
     print('Элементы не найдены')
 
-
-password_field = driver.find_element(By.CSS_SELECTOR, "input#password")
-
 if password_field:
     print('Элементы найдены')
 else:
     print('Элементы не найдены')
-
-
-submit_button = driver.find_element(By.CSS_SELECTOR, "[type='submit']")
 
 if submit_button:
     print('Элементы найдены')
